d2b63144705999b44d796af11b334060f9bbbd4c
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-17 23:34:49 +0000

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - the main code entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{	char small;
+	for (small = 'a'; small <= 'z'; small++)
+	{
+		putchar(small);
+		putchar('\n');
+	}
+	return (0);
+}
