49b4a045c57835cb02ecd98b20d458cfc1872318
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-18 00:38:59 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the code entry point for the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
