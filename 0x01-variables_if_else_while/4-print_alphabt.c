15590fe1e0a8f573f99f492e57358d457a2c43c3
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-18 00:11:58 +0000

    excemption of e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the main code entry point of the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char small;
+	char e;
+	char q;
+
+	e = 'e';
+	q = 'q';
+	for (small = 'a'; small <= 'z'; small++)
+	{
+		if (small != e && small != q)
+		{
+			putchar(small);
+		}
+			putchar('\n');
+	}
+	return (0);
+}
