7c03c5bfdd1c58ee819cab4ec641fd71a2f3bd7a
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-18 00:48:07 +0000

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the code entry of the program
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'z'; b <= 'a'; b--)
+	{
+		putchar(b);
+	}
+		putchar('\n');
+	return (0);
+}
