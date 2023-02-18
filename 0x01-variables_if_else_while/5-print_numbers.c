2b0dccf06a0ca5ff7654dcfc250d12b09d89fb7c
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-18 00:27:18 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - code entry of the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
