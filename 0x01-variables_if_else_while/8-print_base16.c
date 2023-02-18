35d77651f40751d9eb89c5a289b8f98e7428cde3
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-18 01:01:05 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the code entry of the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char h;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+	for (h = 'a'; h <= 'f'; h++)
+	{
+		putchar(h);
+	}
+		putchar('\n');
+	return (0);
+}
