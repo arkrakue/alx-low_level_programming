20b275405ac77725788079b0bb9768e3810c7a2b
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-22 03:47:12 +0000

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - performs addition of two numbers
+ * and retirn the results
+ * @x: number 1
+ * @y: number 2
+ * Return: additon of number 1 and 2
+ */
+int add(int x, int y)
+{
+	int c;
+
+	c = x + y;
+	return (c);
+}
