a605b31dbb45326b0990a4618eccd5079a28cf73
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-24 04:07:22 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,22 +13,23 @@ void print_triangle(int size)
 	int i;
 	int j;
 	int h;
+	int d;
 
-	h = size - 1;
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		h = size - 1;
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j <= size; j++)
+			for (j = h; j > 0; j--)
 			{
-				if (j < h)
-					_putchar(' ');
-				else
-					_putchar('#');
-
+				_putchar(' ');
 			}
-			h--;
-			putchar('\n');
+			for (d = 0; d <= i; d++)
+			{
+				_putchar(35);
+			}
+		h--;
+		_putchar('\n');
 		}
 	}
 	else
