6832e91f87991f75ade988dfff60bb3e030eb27b
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-27 23:02:58 +0000

    strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	int i;
+	char rev;
+
+	x = 0;
+	while (s[x] != '\0')
+		x++;
+	while (x)
+	{
+		rev = s[x];
+		s[i++] = s[x];
+		s[x] = rev;
+	}
+}
