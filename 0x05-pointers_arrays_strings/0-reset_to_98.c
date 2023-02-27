07882bebf3da956e813f4f3afc790b32f39b291c
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-02-27 21:27:54 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to
+ *  to 98
+ *  @n: value of the pointer
+ *  Return: viod
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The files contained here is this directory are those required for the alx project 0x05-pointers_arrays_strings
