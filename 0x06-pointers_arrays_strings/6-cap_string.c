ad458ebfb2e9211e0450053e05bfefed3fad881f
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-02 23:55:31 +0000

    initial capital

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - Does intial capitalization of the string passes
+ * @str: inpust string passed
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		switch (str[i])
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case ' ':
+		case '\n':
+		case '\t':
+			if (str[i + 1] > 96 && str[i + 1] < 123)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+	}
+	return (str);
+}
