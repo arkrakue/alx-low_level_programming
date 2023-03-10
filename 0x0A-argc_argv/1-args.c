1e217afe0ed4b15b4cf964902c22a2e21015bbde
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-10 22:54:42 +0000

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function prints out the number of arguments passed
+ * @argc: the number of argument passed
+ * @argv: the string array of the text in the command line
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+		printf("%d\n", argc);
+	return (0);
+}
