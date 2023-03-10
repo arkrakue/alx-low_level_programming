41aaa60fbdaa6aca711b85fd0e1b125e906ccbc3
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-10 22:06:33 +0000

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the input code of the program
+ * @argc: the value of the agrv array
+ * @argv: array of strings in the command line
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	else
+	{
+	}
+	return (0);
+}
