7ee935c251396d611a7cace07dedf53f7103c377
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-14 19:45:27 +0000

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * str_concat
- * @s1
- * @s2
- * Return
+ * str_concat - concatenates(joins) to strings
+ * @s1: first string to be represented
+ * @s2: second to be represented
+ * Return: pointer to the joint string and NULL is not successful
  */
 
 char *str_concat(char *s1, char *s2)
@@ -16,13 +16,19 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	char *array;
 
+	if (s1 == 0)
+		s1 = "";
 	length_s1 = 0;
 	while (s1[length_s1] != '\0')
 		length_s1++;
+	if (s2 == 0)
+		s2 = "";
 	length_s2 = 0;
 	while (s2[length_s2] != '\0')
 		length_s2++;
 	array = malloc((length_s1 + length_s2 - 1) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
 	{
@@ -32,10 +38,10 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	while (s2[j] != '\0')
 	{
-	       	array[i + j] = s2[j];
+		array[i + j] = s2[j];
 		j++;
 	}
-	array[i+j] = '\0';
+	array[i + j] = '\0';
 	return (array);
 }
 
