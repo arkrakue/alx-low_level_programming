efe12cd83e37dcbe60fc523cf7cfc9c090bf0f19
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-20 22:02:09 +0000

    structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: struct name
+ * @name: name of the dog
+ * @age: its age
+ * @owner: its owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: dog's name
+ * @age: its age
+ * @owner: its owner
+ * Return: pointer of type dog_t and
+ * NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_cpy;
+	char *owner_cpy;
+	int len_name = 0;
+	int len_owner = 0;
+	int i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	name_cpy = malloc(len_name + 1);
+	if (name_cpy == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		name_cpy[i] = name[i];
+	name_cpy[i] = '\0';
+	owner_cpy = malloc(len_owner + 1);
+	if (owner_cpy == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		owner_cpy[i] = owner[i];
+	owner_cpy[i] = '\0';
+	new_dog->name = name_cpy;
+	new_dog->age = age;
+	new_dog->owner = owner_cpy;
+	return (new_dog);
+}
