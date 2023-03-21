5afb76fbb351906d10044f4d76698064c855449b
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-21 00:07:07 +0000

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,41 +13,36 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_cpy;
-	char *owner_cpy;
-	int len_name = 0;
-	int len_owner = 0;
+	int len_name;
+	int len_owner;
 	int i;
+	dog_t *new_dog;
 
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	len_name = len_owner = 0;
 	while (name[len_name])
 		len_name++;
 	while (owner[len_owner])
 		len_owner++;
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-	name_cpy = malloc(len_name + 1);
-	if (name_cpy == NULL)
+	new_dog->name = malloc(len_name * sizeof(new_dog->name));
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
-		name_cpy[i] = name[i];
-	name_cpy[i] = '\0';
-	owner_cpy = malloc(len_owner + 1);
-	if (owner_cpy == NULL)
+	for (i = 0; i <= len_name; i++)
+		new_dog->name[i] = name[i];
+	new_dog->age = age;
+	new_dog->owner = malloc(len_owner * sizeof(new_dog->owner));
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; owner[i]; i++)
-		owner_cpy[i] = owner[i];
-	owner_cpy[i] = '\0';
-	new_dog->name = name_cpy;
-	new_dog->age = age;
-	new_dog->owner = owner_cpy;
+	for (i = 0; i <= len_owner; i++)
+		new_dog->owner[i] = owner[i];
 	return (new_dog);
 }
