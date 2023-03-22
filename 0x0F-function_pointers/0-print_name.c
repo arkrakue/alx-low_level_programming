5e0366ac9d9aa5fcae35e69f1f9cda7afd1edcb6
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-22 21:45:33 +0000

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,7 @@
 /**
  * print_name - print the name in the function argument
  * @f: function that prints name
+ * @name: name to print
  * Return: void
  */
 
@@ -11,5 +12,4 @@ void print_name(char *name, void (*f)(char *))
 	if (name == NULL || f == NULL)
 		return;
 	(*f)(name);
-	return;
 }
