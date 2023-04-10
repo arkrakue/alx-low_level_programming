2ef32ed8fce0d0e823d37d12bc32b2ddecf8770f
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-04-10 11:54:53 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit at aan index to 1
+ * @index: Location of the bit
+ * @n: Decimal representation of the input number
+ * Return: bit's new value
+ */
+
+int set_bit( unsigned long int *n, unsigned int index)
+{
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
+		return (-1);
+	*n |= max;
+	return (1);
+}
