3828b6056ad1c2bddd9e848e8a725e26d7eec9d8
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-04-10 13:19:17 +0000

    advanced task 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks for endianness
+ * Return: 0 for big and 1 for little
+ */
+
+int get_endianness(void)
+{
+	unsigned int j = 1;
+	char *ch = (char *) &j;
+
+	return (*ch);
+}
