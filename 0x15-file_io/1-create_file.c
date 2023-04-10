1b08bde2afa6500733bdc60ba41bdd524e5359e3
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-04-10 18:31:44 +0000

    file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,11 @@ int create_file(const char *filename, char *text_content)
 	filedes = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (filedes < 0)
 		return (-1);
-	while (text_content == NULL)
+	while (text_content  && *(text_content + length))
 		length++;
-	if (text_content)
-		file = write(filedes, text_content, length);
+	file = write(filedes, text_content, length);
 	close(filedes);
-	return (file == -1 ? -1 : 1);
+	if (file < 0)
+		return (-1);
+	return (1);
 }
